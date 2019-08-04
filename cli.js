d2f6a46e564153fb3948878b36c579dfe87f5169
Alex Kladkov <kldkv@imac.imac>
2019-08-04 20:30:40 +0300

    some changes

--- cli.js
+++ cli.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 const path = require('path')
 
-const {convertFiles} = require('./index.js')
+const {main} = require('./index.js')
 
 const args = process.argv;
 
 const DIR_SRC = path.resolve('./', args[2]);
 const DIR_DEST = path.resolve('./', args[3]);
-
 const CUT = args[4] === 'cut';
 
-convertFiles({DIR_SRC, DIR_DEST, CUT})
+main({DIR_SRC, DIR_DEST, CUT})
--- index.js
+++ index.js
@@ -5,69 +5,79 @@ const isEqual = require('lodash.isequal');
 const emojiList = require('emojis-list');
 
 const SPECIAL_SYMBOLS = [0xFE0F, 0x200D];
+const args = process.argv;
+
+const DIR_SRC = path.resolve('./', args[2]);
+const DIR_DEST = path.resolve('./', args[3]);
+
+const CUT = args[4] === 'cut';
 
 const emojiUnicodePoints = emojiList.map((code) => [...code].map((point) => point.codePointAt(0)));
 
-const convertFiles = function({DIR_SRC, DIR_DEST, CUT}) {
-  return fs.readdir(DIR_SRC, (err, files) => {
-    console.time('Converted in')
-    files.forEach(file => {
-      const [name, ext] = file.split('.'); // '1f3c3-1f3ff-2642.png' -> ['1f3c3-1f3ff-2642', '.png']
-      if (!name || !ext) return;
+function main({DIR_SRC, DIR_DEST, CUT}) {  
+  function convertFiles() {
+    return fs.readdir(DIR_SRC, (err, files) => {
+      console.time('Converted in')
+      files.forEach(file => {
+        const [name, ext] = file.split('.'); // '1f3c3-1f3ff-2642.png' -> ['1f3c3-1f3ff-2642', '.png']
+        if (!name || !ext) return;
 
-      const points = name
-        .split('-') // '1f3c3-1f3ff-2642' -> ['1f3c3', '1f3ff', '2642']
-        .map(code => parseInt(code, 16)) // ['1f3c3', '1f3ff', '2642'] -> [127939, 127999, 9794]
-        .filter((code) => !SPECIAL_SYMBOLS.includes(code)); // [127939, 127999, 9794]
+        const points = name
+          .split('-') // '1f3c3-1f3ff-2642' -> ['1f3c3', '1f3ff', '2642']
+          .map(code => parseInt(code, 16)) // ['1f3c3', '1f3ff', '2642'] -> [127939, 127999, 9794]
+          .filter((code) => !SPECIAL_SYMBOLS.includes(code)); // [127939, 127999, 9794]
 
-      const equal = (codes) => isEqual(codes, points);
+        const equal = (codes) => isEqual(codes, points);
 
-      const filtredEmojis = emojiUnicodePoints // [..., [127939, 127999, 8205, 9794, 65039], ...]
-        // [..., [127939, 127999, 9794], ...]
-        .filter((codes) => equal(codes.filter((code) => !SPECIAL_SYMBOLS.includes(code))))
+        const filtredEmojis = emojiUnicodePoints // [..., [127939, 127999, 8205, 9794, 65039], ...]
+          // [..., [127939, 127999, 9794], ...]
+          .filter((codes) => equal(codes.filter((code) => !SPECIAL_SYMBOLS.includes(code))))
 
-      if (filtredEmojis.length === 1) {
-        copy({file, pointsEmoji: filtredEmojis[0], DIR_SRC, DIR_DEST, CUT})
-      } else {
-        console.log(`Not supported in Emoji 12.0 or something went wrong with: ${file}`)
+        if (filtredEmojis.length === 1) {
+          copy({file, pointsEmoji: filtredEmojis[0], DIR_SRC, DIR_DEST, CUT})
+        } else {
+          console.log(`Not supported in Emoji 12.0 or something went wrong with: ${file}`)
+        }
+      });
+
+      emojiUnicodePoints.forEach((emojiCode) => {
+        const emoji = String.fromCodePoint(...emojiCode)
+        const exist = fs.existsSync(`${DIR_DEST}/${emoji}.png`) || fs.existsSync(`${DIR_DEST}/${emoji}.svg`)
+
+        if (!exist) {
+          console.log(`Not found file for emoji: ${emoji}`)
+        }
+      })
+
+      console.timeEnd('Converted in')
+    });
+  };
+  
+  function copy({file, pointsEmoji}) {
+    const [name, ext] = file.split('.');
+
+    const emoji = String.fromCodePoint(...pointsEmoji);
+    const filePath = path.resolve(DIR_SRC, file);
+    const emojiPath = path.resolve(DIR_DEST, `${emoji}.${ext}`)
+
+    fs.copyFile(filePath, emojiPath, fs.constants.COPYFILE_EXCL, (err) => {
+      if (err) {
+        console.log({emoji, pointsEmoji})
+
+        throw err;
+      }
+
+      if (CUT) {
+        fs.unlinkSync(filePath)
       }
     });
 
-    emojiUnicodePoints.forEach((emojiCode) => {
-      const emoji = String.fromCodePoint(...emojiCode)
-      const exist = fs.existsSync(`${DIR_DEST}/${emoji}.png`)
+    return;
+  }
 
-      if (!exist) {
-        console.log(`Not found file for emoji: ${emoji}`)
-      }
-    })
-
-    console.timeEnd('Converted in')
-  });
-};
-
-function copy({file, pointsEmoji, DIR_SRC, DIR_DEST, CUT}) {
-  const [name, ext] = file.split('.');
-
-  const emoji = String.fromCodePoint(...pointsEmoji);
-  const filePath = path.resolve(DIR_SRC, file);
-  const emojiPath = path.resolve(DIR_DEST, `${emoji}.${ext}`)
-
-  fs.copyFile(filePath, emojiPath, fs.constants.COPYFILE_EXCL, (err) => {
-    if (err) {
-      console.log({emoji, pointsEmoji})
-
-      throw err;
-    }
-
-    if (CUT) {
-      fs.unlinkSync(filePath)
-    }
-  });
-
-  return;
+  convertFiles()
 }
 
 module.exports = {
-  convertFiles
+  main
 }
