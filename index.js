4126eb50ae0f0f2c23ea0d8786e38e040b5efbb3
Alex Kladkov <kldkv@imac.imac>
2019-08-31 15:56:30 +0300

    Refactoring filter function

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ const DIR_DEST = path.resolve('./', args[3]);
 const CUT = args[4] === 'cut';
 
 const emojiUnicodePoints = emojiList.map((code) => [...code].map((point) => point.codePointAt(0)));
+const omitSymbols = (code) => !SPECIAL_SYMBOLS.includes(code);
 
 function main({DIR_SRC, DIR_DEST, CUT}) {  
   function convertFiles() {
     return fs.readdir(DIR_SRC, (err, files) => {
       console.time('Converted in')
+      
       files.forEach(file => {
         const [name, ext] = file.split('.'); // '1f3c3-1f3ff-2642.png' -> ['1f3c3-1f3ff-2642', '.png']
         if (!name || !ext) return;
@@ -25,13 +27,13 @@ function main({DIR_SRC, DIR_DEST, CUT}) {
         const points = name
           .split('-') // '1f3c3-1f3ff-2642' -> ['1f3c3', '1f3ff', '2642']
           .map(code => parseInt(code, 16)) // ['1f3c3', '1f3ff', '2642'] -> [127939, 127999, 9794]
-          .filter((code) => !SPECIAL_SYMBOLS.includes(code)); // [127939, 127999, 9794]
+          .filter(omitSymbols); // [127939, 127999, 9794]
 
         const equal = (codes) => isEqual(codes, points);
 
         const filtredEmojis = emojiUnicodePoints // [..., [127939, 127999, 8205, 9794, 65039], ...]
           // [..., [127939, 127999, 9794], ...]
-          .filter((codes) => equal(codes.filter((code) => !SPECIAL_SYMBOLS.includes(code))))
+          .filter((codes) => equal(codes.filter(omitSymbols)))
 
         if (filtredEmojis.length === 1) {
           copy({file, pointsEmoji: filtredEmojis[0], DIR_SRC, DIR_DEST, CUT})
@@ -42,7 +44,8 @@ function main({DIR_SRC, DIR_DEST, CUT}) {
 
       emojiUnicodePoints.forEach((emojiCode) => {
         const emoji = String.fromCodePoint(...emojiCode)
-        const exist = fs.existsSync(`${DIR_DEST}/${emoji}.png`) || fs.existsSync(`${DIR_DEST}/${emoji}.svg`)
+        const fileName = `${DIR_DEST}/${emoji}`;
+        const exist = fs.existsSync(`${fileName}.png`) || fs.existsSync(`${fileName}.svg`)
 
         if (!exist) {
           console.log(`Not found file for emoji: ${emoji}`)
